2d6b3a592b2c3b774cecb6a984b0e3c947763a8e
kevwilhelm95 <kwilhelm95@gmail.com>
2024-06-12 16:35:40 -0500

    Update version

--- pyCfS/__init__.py
+++ pyCfS/__init__.py
@@ -9,5 +9,5 @@ from .Structure import lollipop_plot
 
 from .utils import _hypergeo_overlap, _load_grch38_background, _load_string, _load_reactome, _get_open_targets_gene_mapping, _define_background_list, _clean_genelists, _format_scientific, _fix_savepath, _select_evidences, _get_evidence_types, _get_combined_score, _get_edge_weight, _load_clean_string_network
 
-__version__ = '0.0.11'
+__version__ = '0.0.12'
 __author__ = 'Kevin Wilhelm, Jenn Asmussen'
