ede0d2d9e508ec04cfd5dd1af4f8016b3a6194a6
kevwilhelm95 <kwilhelm95@gmail.com>
2024-07-08 21:37:59 -0500

    v0.0.14 - Add odds_ratios() & ea_distributions()

--- pyCfS/__init__.py
+++ pyCfS/__init__.py
@@ -9,5 +9,5 @@ from .Structure import lollipop_plot, protein_structures
 
 from .utils import _hypergeo_overlap, _load_grch38_background, _load_string, _load_reactome, _get_open_targets_gene_mapping, _define_background_list, _clean_genelists, _format_scientific, _fix_savepath, _select_evidences, _get_evidence_types, _get_combined_score, _get_edge_weight, _load_clean_string_network
 
-__version__ = '0.0.13'
+__version__ = '0.0.14'
 __author__ = 'Kevin Wilhelm, Jenn Asmussen'
