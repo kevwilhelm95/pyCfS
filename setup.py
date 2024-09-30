3b3d80adc70765fce0da69dee8ffbfd17e409f8e
kevwilhelm95 <kwilhelm95@gmail.com>
2024-09-30 07:32:38 -0500

    v0.0.15.14 - Fix error parsing risk_prediction best model

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ Setup script for pyCfS, a package for gene list validation experiments
 """
 import setuptools
 
-VERSION = '0.0.15.13'
+VERSION = '0.0.15.14'
 
 setuptools.setup(
     name = 'pyCfS',
