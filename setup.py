ca5cc33c8efde9621e751794bbf77c84a4b713d5
kevwilhelm95 <kwilhelm95@gmail.com>
2024-10-23 11:20:41 -0500

    v0.0.15.18 - Structure functions
    - Lollipop Plots -- Added text saving of Odds Ratio and p-values
    - Protein Structures -- Fixed error when parsing amino acid positions from frameshift variants with premature stops

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ Setup script for pyCfS, a package for gene list validation experiments
 """
 import setuptools
 
-VERSION = '0.0.15.17'
+VERSION = '0.0.15.18'
 
 setuptools.setup(
     name = 'pyCfS',
